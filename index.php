ce088c265376b57dc28d8d704cb97ddac879bc45
Lip-7 <zhighofficial@gmail.com>
2023-05-16 15:11:12 +0200

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- <?php ?> -->
+<?php 
+
+?>
+
+
+<?php 
+include __DIR__ . './partials/templates/header.php';
+include __DIR__ . './partials/templates/main.php';
+include __DIR__ . './partials/templates/footer.php';
+?>
