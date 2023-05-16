bcf21c33cc3d6321005c562c1a8b88f30d09aa35
Lip-7 <zhighofficial@gmail.com>
2023-05-16 17:49:06 +0200

    mmmm

--- partials/server/movies.php
+++ partials/server/movies.php
@@ -2,9 +2,18 @@
 include __DIR__ . './settings.php';
 include __DIR__ . '../../Models/Movie.php';
 
+/* $newMovies = [
+    new Movie(11, 'Shutter island', 'Shutter island', 'en', '2010-05-24', 9.5, 'https://pad.mymovies.it/filmclub/2009/03/099/locandina.jpg', 'thriller'),
+    new Movie(12, 'La grande Bellezza', 'La grande Bellezza', 'it', '2013-03-22', 9.9, 'https://pad.mymovies.it/filmclub/2012/05/105/locandina.jpg', ['bello', 'bellissimo']),
+]; */
 $newMovies = [
     new Movie(11, 'Shutter island', 'Shutter island', 'en', '2010-05-24', 9.5, 'https://pad.mymovies.it/filmclub/2009/03/099/locandina.jpg', 'thriller'),
     new Movie(12, 'La grande Bellezza', 'La grande Bellezza', 'it', '2013-03-22', 9.9, 'https://pad.mymovies.it/filmclub/2012/05/105/locandina.jpg', ['bello', 'bellissimo']),
 ];
 
-$popularMovies = Movie::fetchAll($conn);
+/* $result = Movie::fetchAll($conn);
+$popularMovies = [];
+while ($row = $result->fetch_object('Movie')) {
+    $oldMovies[] = $row;
+}
+var_dump($oldMovies); */
